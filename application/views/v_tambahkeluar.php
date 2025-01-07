cb89b661c3a24ba130499d7ad8721aea09415e81
Gilang Rizky Ramadhan <94603976+gilangrizkyr@users.noreply.github.com>
2025-01-07 13:10:33 +0700

    Update v_tambahkeluar.php
    perbaikan

--- application/views/v_tambahkeluar.php
+++ application/views/v_tambahkeluar.php
@@ -1,6 +1,4 @@
 <div class="container-fluid">
-    <!-- Page Heading -->
-    <!-- DataTales Example -->
     <div class="card shadow mb-4">
         <div class="card-header py-3">
             <h6 class="m-0 font-weight-bold text-primary">Tambah Barang Keluar</h6>
@@ -8,38 +6,50 @@
         <div class="card-body">
             <div class="col-6">
                 <form action="<?= base_url('barangkeluar/tambahkeluar_proses') ?>" method="post">
-
                     <div class="form-group">
                         <label for="nama_barang">Nama Barang</label>
-                        <input type="text" name="nama_barang" class="form-control" id="nama_barang" required>
+                        <input type="text" name="nama_barang" class="form-control" id="nama_barang" onkeyup="searchBarang()" required>
                     </div>
-
+                    <div id="hasilPencarian" style="margin-top: 10px;"></div>
                     <div class="form-group">
-                        <label for="spesifikasi">Spesifikasi</label><br>
-                        <input type="radio" name="spesifikasi" value="TK" id="spesifikasi_tk">
-                        <label for="spesifikasi_tk">TK</label>
-                        <input type="radio" name="spesifikasi" value="SD" id="spesifikasi_sd">
-                        <label for="spesifikasi_sd">SD</label>
-                        <input type="radio" name="spesifikasi" value="SMP" id="spesifikasi_smp">
-                        <label for="spesifikasi_smp">SMP</label>
-                        <input type="radio" name="spesifikasi" value="MTS" id="spesifikasi_mts">
-                        <label for="spesifikasi_mts">MTS</label>
-                        <input type="radio" name="spesifikasi" value="SMA" id="spesifikasi_sma">
-                        <label for="spesifikasi_sma">SMA</label>
-                        <input type="radio" name="spesifikasi" value="SMK" id="spesifikasi_smk">
-                        <label for="spesifikasi_smk">SMK</label>
+                        <label for="spesifikasi">Spesifikasi</label>
+                        <input type="text" name="spesifikasi" class="form-control" id="spesifikasi" readonly required>
                     </div>
-
                     <div class="form-group">
                         <label for="jumlah">Jumlah</label>
                         <input type="number" name="jumlah" class="form-control" id="jumlah" required>
                     </div>
-
                     <br>
                     <button type="submit" class="btn btn-success btn-sm">Simpan</button>
-
                 </form>
             </div>
         </div>
     </div>
 </div>
+
+<script>
+    function searchBarang() {
+        let namaBarang = document.getElementById('nama_barang').value;
+
+        if (namaBarang.length < 3) {
+            document.getElementById('hasilPencarian').innerHTML = '';
+            return;
+        }
+
+        const xhr = new XMLHttpRequest();
+        xhr.open('GET', '<?= base_url('barangkeluar/search_barang'); ?>?nama_barang=' + namaBarang, true);
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState == 4 && xhr.status == 200) {
+                document.getElementById('hasilPencarian').innerHTML = xhr.responseText;
+            }
+        };
+        xhr.send();
+    }
+
+    function selectBarang(namaBarang, spesifikasi) {
+        // Mengisi input nama_barang dan spesifikasi
+        document.getElementById('nama_barang').value = namaBarang;
+        document.getElementById('spesifikasi').value = spesifikasi;
+        document.getElementById('hasilPencarian').innerHTML = ''; // Hapus hasil pencarian setelah pemilihan
+    }
+</script>
